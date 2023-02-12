03d2fa0f76bda3fdcb256445a888c042510683e7
Marco Portacio <marcoportacio4@gmail.com>
2023-02-12 14:12:41 -0500

    Se ha corregido un pequeño error de espaciado en la pantalla de creación de una tarea

--- app/src/main/java/com/markusw/app/ui/view/screens/writtetodo/composables/TaskHourField.kt
+++ app/src/main/java/com/markusw/app/ui/view/screens/writtetodo/composables/TaskHourField.kt
@@ -2,11 +2,8 @@ package com.markusw.app.ui.view.screens.writtetodo.composables
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
